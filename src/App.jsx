0f82787aadfda8f83e7ef9ed9c70c42e731e2026
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-08-18 04:05:24 -0700

    feat: Complete Navbar and Info components with styiling

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Chart from "./components/Chart"
 import DoughnutChart from "./components/DoughnutChart"
 import Transactions from "./components/Transactions"
 import Accounts from "./components/Accounts"
+import  useStore  from "./store/index.js"
 
 function App() {
-  const theme = "white";
+  const theme = useStore((state) => state.theme);
   return (
     <main className={theme} >
       <div className="w-full px-6 md:px-20 bg-white dark:bg-slate-900">
         <Navbar />
 
         <div className="px-0 md:px-5 2xl:px-20">
-          <Info />
+          <Info 
+            title="Dashboard"
+            subTitle="Monitor your financial activities"
+          />
           <Stats/>
         </div>
 
---
+++ src/components/ThemeSwitch.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import useStore from '../store/index.js'
+
+const ThemeSwitch = () => {
+  const {theme, setTheme} = useStore((state) => state)
+  const [isDarkMode , setIsDarkMode] = useState(theme === "dark")
+  
+  const toggleTheme = ( ) => {
+    const newTheme = isDarkMode ? "light" : "dark";
+    setIsDarkMode(!isDarkMode);
+    
+    setTheme(newTheme);
+    localStorage.setItem("theme" , newTheme)
+  };
+
+
+  return (
+    <div 
+      className={`switch ${isDarkMode ? 'light' : 'dark'}`}
+      onClick={toggleTheme}
+    >
+      <div className={`ball ${isDarkMode ? "dark": "light"}`}></div>
+    </div>
+  )
+}
+
+export default ThemeSwitch
